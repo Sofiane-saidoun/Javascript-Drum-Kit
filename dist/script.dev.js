fda13dcb30dc59c673d3a44f1982e89b867bc164
Sofiane <sofiane.saidoun@gmail.com>
2020-09-06 17:57:43 +0200

    finished

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,3 +1,15 @@
 "use strict";
 
-console.log("hello word");
+function playSound(e) {
+  var audio = document.querySelector("audio[data-key=\"".concat(e.keyCode, "\"]"));
+  var key = document.querySelector(".key[data-key=\"".concat(e.keyCode, "\"]"));
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add("play");
+  key.addEventListener("transitionend", function () {
+    key.classList.remove("play");
+  });
+}
+
+window.addEventListener("keydown", playSound);
--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("hello word");
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+  if (!audio) return;
+
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add("play");
+  key.addEventListener("transitionend", function () {
+    key.classList.remove("play");
+  });
+}
+
+window.addEventListener("keydown", playSound);
